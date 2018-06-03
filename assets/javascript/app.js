9e97e608fd31fc606e8a7216aa590965482cb6b5
Han <2han@live.com>
2018-06-02 22:20:51 -0700

    added index

---
+++ assets/javascript/app.js
@@ -0,0 +1,12 @@
+var config = {
+    apiKey: "AIzaSyCDbm5EXDkE_zS2rNvliaBSefARlDvVLr0",
+    authDomain: "myexampleproj-a46aa.firebaseapp.com",
+    databaseURL: "https://myexampleproj-a46aa.firebaseio.com",
+    projectId: "myexampleproj-a46aa",
+    storageBucket: "myexampleproj-a46aa.appspot.com",
+    messagingSenderId: "169778421609"
+  };
+  firebase.initializeApp(config);
+
+var database = firebase.database();
+
